a33ff682ffa94dacdab45b009f2f4b13deff6b4e
Ismail Minjasar <dcoder1745@gmail.com>
2021-07-05 05:43:22 +0600

    05:43 05.07.2021

--- src/controllers/profileEdit.jsx
+++ src/controllers/profileEdit.jsx
@@ -1,7 +1,41 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+import { store } from 'store';
+import { setAuth } from 'store/userReducer';
+
+import Cookies from 'universal-cookie';
+
 import View from 'views/profileEdit';
 
 const ProfileEdit = () => {
-    return <View/>;
+    const [user, setUser] = useState({});
+    const [isLoading, setLoading] = useState(false);
+
+    useEffect(() => {
+        getProfile(setUser, setLoading);
+        
+        return () => {
+            setUser({});
+            setLoading(false);
+        };
+    }, []);
+
+    if(!isLoading) return null;
+    return <View data={user}/>;
+}
+
+const getProfile = async (setUser, setLoading) => {
+    const cookies = new Cookies();
+    const response = (await axios.post('/account/get/' + cookies.get('user_id'), {token: cookies.get('token')})).data
+
+    console.log(response);
+
+    if(response.status) {
+        setUser(response.data)
+        setLoading(true);
+    }
 }
 
 export default ProfileEdit;
--- src/views/profileEdit/index.jsx
+++ src/views/profileEdit/index.jsx
@@ -1,9 +1,36 @@
+import { useState } from 'react';
 import './profileEdit.css';
 
-const ProfileEdit = () => {
+const ProfileEdit = ({data}) => {
+    const [avatar, setAvatar] = useState('https://api.daiyndyq.kz/' + data.avatar);
+    const [firstName, setFirstName] = useState(data.first_name);
+    const [lastName, setLastName] = useState(data.last_name);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log({firstName, lastName})
+    }
+
     return (
         <section className='profile-edit-box box container'>
-            s
+            <form onSubmit={onSubmit} className='profile-edit-block'>
+                <div className='profile-edit-avatar'>
+                    <img src={avatar} alt='avatar'/>
+                    <label className='profile-edit-avatar-new button' htmlFor='upload-avatar'>Жаңасын жүктеу</label>
+                    <input type='file' id='upload-avatar' onChange={e => setAvatar(URL.createObjectURL(e.target.files[0]))}/>
+                </div>
+                <div className='profile-edit-data'>
+                    <div className='profile-edit-item'>
+                        <label>Есіміңіз:</label>
+                        <input type='text' placeholder='Есіміңіз' value={firstName} onChange={e => setFirstName(e.target.value)}/>
+                    </div>
+                    <div className='profile-edit-item'>
+                        <label>Тегіңіз:</label>
+                        <input type='text' placeholder='Тегіңіз' value={lastName} onChange={e => setLastName(e.target.value)}/>
+                    </div>
+                    <button className='button' type='submit'>Сақтау</button>
+                </div>
+            </form>
         </section>
     );
 }
